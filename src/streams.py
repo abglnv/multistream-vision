3f3175d2aae5c544079c3870faa398e1319a205a
rendizi <baglanov.a0930@gmail.com>
2026-04-26 09:09:04 +0500

    upd: new streams

--- src/streams.py
+++ src/streams.py
@@ -1,14 +1,6 @@
-BASE_URL = "https://kaztoll.kz/livestream"
-
 STREAMS = [
-    f"{BASE_URL}/montitatas.mp4",
-    f"{BASE_URL}/kazygurt.mp4",
-    f"{BASE_URL}/atameken.mp4",
-    f"{BASE_URL}/cam4.mp4",
-    f"{BASE_URL}/jjvezd.mp4",
-    f"{BASE_URL}/arshaly.mp4",
-    f"{BASE_URL}/anar.mp4",
-    f"{BASE_URL}/oshagandy.mp4",
-    f"{BASE_URL}/ttvezd.mp4",
-    f"{BASE_URL}/kunaev.mp4",
+    "rtsp://45.132.87.124:8680/rtsp/747706/8633143b8cd85556b060",   # 1920x1080 15fps
+    "rtsp://45.132.87.126:8240/rtsp/2087869/9ca8320a474324d8aeb7",  # 1280x720  20fps
+
+    "https://cam.xcom.kz:8081/9ring/tracks-v1/index.m3u8",         # 2560x1440 20fps
 ]
